34091c5268a8d3d40d235d66de2bc88a5e5f0bfa
chenxuan <523764720@qq.com>
2019-08-22 16:27:20 +0800

    感觉有很多业务逻辑要写啊

--- src_in_browser/Main_app.js
+++ src_in_browser/Main_app.js
@@ -6,7 +6,24 @@ class Main_app {
     constructor() {
         this.term = new xterm_1.Terminal();
         this.term.open(document.getElementById('terminal_ui'));
+        this.init_listener();
         this.fit_screen();
+    }
+    init_listener() {
+        this.term.on("data", (data) => {
+            this.term.write(data);
+        });
+        this.term.on("key", (key, ev) => {
+            if (ev.keyCode == 13) {
+                this.prompt();
+            }
+        });
+        this.term.on('paste', (data) => {
+            this.term.write(data);
+        });
+    }
+    prompt() {
+        this.term.write("\r\n-->: ");
     }
     fit_screen() {
         fit_1.fit(this.term);
--- src_in_browser/Main_app.ts
+++ src_in_browser/Main_app.ts
@@ -8,7 +8,32 @@ class Main_app
     {
         this.term = new Terminal();
         this.term.open(<HTMLDivElement>document.getElementById('terminal_ui'))
+        this.init_listener()
         this.fit_screen()
+    }
+
+    init_listener()
+    {
+        this.term.on("data", (data: string) =>
+        {
+            this.term.write(data)
+        })
+        this.term.on("key", (key:string, ev: KeyboardEvent) =>
+        {
+            if(ev.keyCode == 13)
+            {
+                this.prompt()
+            }
+        })
+        this.term.on('paste', (data) =>
+        {
+            this.term.write(data);
+        });
+    }
+
+    prompt()
+    {
+        this.term.write("\r\n-->: ")
     }
     
     fit_screen()
