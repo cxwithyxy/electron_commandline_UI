f054e7afbd9d7e5f873771baecefb39dfccff75f
chenxuan <523764720@qq.com>
2019-08-23 14:07:51 +0800

    简单模拟了终端, 但是不够完整

--- src_in_browser/Main_app.js
+++ src_in_browser/Main_app.js
@@ -4,26 +4,57 @@ const xterm_1 = require("xterm");
 const fit_1 = require("xterm/lib/addons/fit/fit");
 class Main_app {
     constructor() {
+        this.prompt_str = "-->: ";
         this.term = new xterm_1.Terminal();
         this.term.open(document.getElementById('terminal_ui'));
         this.init_listener();
         this.fit_screen();
+        this.prompt();
     }
     init_listener() {
-        this.term.on("data", (data) => {
-            this.term.write(data);
-        });
+        // this.term.on("data", (data: string) =>
+        // {
+        //     console.log(JSON.stringify({a:data}))
+        //     if(!(data == ""))
+        //     {
+        //         // this.term.write(data)
+        //     }
+        // })
+        // this.term.on("linefeed", () =>
+        // {
+        //     this.prompt()
+        // })
         this.term.on("key", (key, ev) => {
             if (ev.keyCode == 13) {
                 this.prompt();
+                console.log(this.term.rows);
             }
+            else if (ev.keyCode == 8
+                && this.term.buffer.cursorX > this.prompt_str.length) {
+                this.term.write("\b \b");
+            }
+            else {
+                this.term.write(key);
+            }
+            console.log(ev.keyCode);
+        });
+        this.term.attachCustomKeyEventHandler((ev) => {
+            if ([38, 40 /* 37, 39 */].indexOf(ev.keyCode) != -1) {
+                console.log(`方向键被按下`);
+                return false;
+            }
+            if (ev.keyCode == 37
+                && this.term.buffer.cursorX <= this.prompt_str.length) {
+                return false;
+            }
+            return true;
         });
         this.term.on('paste', (data) => {
             this.term.write(data);
         });
     }
     prompt() {
-        this.term.write("\r\n-->: ");
+        this.term.write(`\r\n${this.prompt_str}`);
     }
     fit_screen() {
         fit_1.fit(this.term);
--- src_in_browser/Main_app.ts
+++ src_in_browser/Main_app.ts
@@ -4,26 +4,67 @@ import {fit} from "xterm/lib/addons/fit/fit";
 class Main_app
 {
     term: Terminal
+    prompt_str: string
     constructor()
     {
+        this.prompt_str = "-->: "
         this.term = new Terminal();
         this.term.open(<HTMLDivElement>document.getElementById('terminal_ui'))
         this.init_listener()
         this.fit_screen()
+        this.prompt()
     }
 
     init_listener()
     {
-        this.term.on("data", (data: string) =>
-        {
-            this.term.write(data)
-        })
+        // this.term.on("data", (data: string) =>
+        // {
+        //     console.log(JSON.stringify({a:data}))
+        //     if(!(data == ""))
+        //     {
+        //         // this.term.write(data)
+        //     }
+        // })
+        // this.term.on("linefeed", () =>
+        // {
+        //     this.prompt()
+        // })
         this.term.on("key", (key:string, ev: KeyboardEvent) =>
         {
             if(ev.keyCode == 13)
             {
                 this.prompt()
+                console.log(this.term.rows)
             }
+            else if
+            (
+                ev.keyCode == 8
+                && this.term.buffer.cursorX > this.prompt_str.length
+            ){
+                this.term.write("\b \b")
+            }
+            else
+            {
+                this.term.write(key)
+            }
+            
+            console.log(ev.keyCode)
+        
+        })
+        this.term.attachCustomKeyEventHandler((ev: KeyboardEvent) =>
+        {
+            if([38, 40 /* 37, 39 */].indexOf(ev.keyCode) != -1)
+            {
+                console.log(`方向键被按下`)
+                return false
+            }
+            if(
+                ev.keyCode == 37 
+                && this.term.buffer.cursorX <= this.prompt_str.length
+            ){
+                return false
+            }
+            return true
         })
         this.term.on('paste', (data) =>
         {
@@ -33,7 +74,7 @@ class Main_app
 
     prompt()
     {
-        this.term.write("\r\n-->: ")
+        this.term.write(`\r\n${this.prompt_str}`)
     }
     
     fit_screen()
